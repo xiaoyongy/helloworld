8e21dcbcb7b4c3ded7e9a649749a8dab56a07303
xiaoyongy <xiaoyongy0@gmail.com>
2020-01-05 22:55:42 +0800

    consider the number of k.

--- src/Knil/Rotate.java
+++ src/Knil/Rotate.java
@@ -9,19 +9,20 @@ class ListNode{
 }
 public class Rotate {
     public static ListNode rotateList(ListNode list, int k){
+        ListNode pre = new ListNode(0);
+        ListNode r = list;
         if(list==null||list.next==null||k==0)
             return list;
         int count = 0;
-        while(list!=null){
+        while(r!=null){
             count++;
-            list = list.next;
+            r = r.next;
         }
 //        对k的值进行判断，是否大于链表长度
         if(k%count == 0)
             return list;
         k = k%count;
 
-        ListNode pre = new ListNode(0);
         ListNode tmp = list;
         ListNode temp = list;
         for(int i=0;i<k;i++){
@@ -50,7 +51,7 @@ public class Rotate {
         node3.next = node4;
         node4.next = node5;
 
-        ListNode list = rotateList(node1, 3);
+        ListNode list = rotateList(node1, 7);
         while (list!=null){
             System.out.println(list.val);
             list = list.next;
